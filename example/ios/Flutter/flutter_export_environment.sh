9c8cd577ad4ac45466edc484dfb3253fa45f098e
Richard Muragijimana <beastar457@gmail.com>
2021-11-27 07:19:30 +0200

    main

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\Sone\Documents\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\demos\experiments\flipper\open-sources\flutter_money_formatter\example"
+export "FLUTTER_APPLICATION_PATH=E:\demos\experiments\flipper\open-sources\money_formatter\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
